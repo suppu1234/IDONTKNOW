b5e9ce43ae65f69f7aea6fbaf38524124e6b528d
Su <ssvagle@gmail.com>
2019-12-11 11:58:09 +0530

    added new feature

---
+++ src/com/qa/client/appendnewfile.java
@@ -0,0 +1,10 @@
+package com.qa.client;
+
+public class appendnewfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("new file added");
+	}
+
+}
---
+++ src/com/qa/client/login.java
@@ -0,0 +1,10 @@
+package com.qa.client;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hiii");
+	}
+
+}
