19dc570dfcb7b56b64017f0703b2dfe9dab45c4f
Su <ssvagle@gmail.com>
2019-12-11 11:23:32 +0530

    add new

---
+++ src/com/qa/client/DemoGit.java
@@ -0,0 +1,12 @@
+package com.qa.client;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a Git Demo");
+
+	}
+
+}
